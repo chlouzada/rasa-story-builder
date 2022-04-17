aac341c2fec7f9580c317f7e8342024754abdd36
carlos <chlouzada@gmail.com>
2022-04-17 20:19:37 -0300

    chore: auto scroll to bottom on steps add

--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -1,15 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNlu } from "../../contexts/NluContext";
 import Action from "../Action";
 import Button from "../Button";
 import Intent from "../Intent";
-import Steps from "../Steps";
+// import Steps from "../Steps";
+
+const AlwaysScrollToBottom = () => {
+  const elementRef = useRef<HTMLDivElement>(null);
+  useEffect(() => elementRef.current!.scrollIntoView());
+  return <div ref={elementRef} />;
+};
 
 export default function Story() {
   const [name, setName] = useState("");
   const [steps, setSteps] = useState<JSX.Element[]>([]);
 
-  const stepsRef = React.useRef<HTMLDivElement>(null);
+  const stepsRef = useRef<HTMLDivElement>(null);
 
   const { nlu } = useNlu();
 
@@ -18,14 +24,14 @@ export default function Story() {
       nlu.intents[Math.floor(Math.random() * nlu.intents.length)];
 
     setSteps([...steps, <Intent name={randomIntent.name} />]);
-
-    stepsRef.current!.scrollTop = stepsRef.current!.scrollHeight - 1 || 0;
   };
   const addAction = () => {
     setSteps([...steps, <Action />]);
-    const story = document.getElementById("steps-div");
-    story!.scrollTop = story!.scrollHeight + 1;
   };
+
+  // useEffect(() => {
+  //   // stepsRef.current!.scrollTop = stepsRef.current!.scrollHeight;
+  // }, [steps]);
 
   return (
     <div className="flex flex-col h-full">
@@ -35,12 +41,9 @@ export default function Story() {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <div
-        ref={stepsRef}
-        id="steps-div"
-        className="flex flex-col h-full overflow-auto"
-      >
+      <div ref={stepsRef} className="flex flex-col h-full overflow-auto">
         {steps}
+        <AlwaysScrollToBottom />
       </div>
 
       <div className="flex justify-center">

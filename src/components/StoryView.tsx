6720affd38ee508ffd0721fa21efa76a1818836b
carlos <chlouzada@gmail.com>
2022-09-29 16:55:59 -0300

    add scroll to bottom

--- src/components/StoryView.tsx
+++ src/components/StoryView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { createRef, useEffect, useRef, useState } from 'react';
 import { useDroppable } from '@dnd-kit/core';
 import { useStoryStore } from '../stores/story';
 
@@ -16,11 +16,7 @@ const Droppable: React.FC<DroppableProps> = (props) => {
   };
 
   return (
-    <div
-      ref={setNodeRef}
-      style={{ ...style }}
-      className="flex flex-col grow"
-    >
+    <div ref={setNodeRef} style={{ ...style }} className="flex flex-col grow">
       {props.children}
     </div>
   );
@@ -34,15 +30,27 @@ const StoryItem: React.FC<{ name: string }> = ({ name }) => {
   );
 };
 
+const ScrollToBottom = () => {
+  const { steps } = useStoryStore();
+  const ref = useRef<HTMLDivElement>(null!);
+  useEffect(() => {
+    ref.current.scrollIntoView({ behavior: 'auto' });
+  }, []);
+  useEffect(() => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  }, [steps]);
+  return <div ref={ref} />;
+};
+
 export const StoryView = ({ className }: { className?: string }) => {
   const { steps } = useStoryStore();
-
   return (
     <div className={`${className} overflow-auto`}>
       <Droppable id="story-container">
         {steps.map((step) => (
           <StoryItem {...step} />
         ))}
+        <ScrollToBottom />
       </Droppable>
     </div>
   );

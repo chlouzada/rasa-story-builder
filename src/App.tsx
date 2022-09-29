c35fe91109f72ca5ff17170201f18af77071c886
carlos <chlouzada@gmail.com>
2022-09-29 14:27:49 -0300

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
+const ActionsContainer = ({ className }: { className?: string }) => {
+  return (
+    <div className={className}>
+      <h2>Actions Container</h2>
+      <div></div>
+    </div>
+  );
+};
+
+const IntentsContainer = ({ className }: { className?: string }) => {
+  return (
+    <div className={className}>
+      <h2>Intent Container</h2>
+      <div></div>
+    </div>
+  );
+};
+
+const StoryContainer = ({ className }: { className?: string }) => {
+  return (
+    <div className={className}>
+      <h2>Story Container</h2>
+      <div></div>
+    </div>
+  );
+};
+
 export const App = () => {
-  return <div className='bg-red-500 font-bold'>App</div>;
+  return (
+    <>
+      <h1>Story Builder</h1>
+
+      <main className="grid grid-cols-10 h-full">
+        <ActionsContainer className="col-start-1 col-end-3" />
+        <IntentsContainer className="col-start-3 col-end-9 " />
+        <StoryContainer className="col-start-9 col-end-11" />
+      </main>
+    </>
+  );
 };

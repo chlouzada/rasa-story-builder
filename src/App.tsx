fa7c919d548791427e8764736c6dd7016e6ce6b3
carlos <chlouzada@gmail.com>
2022-09-29 18:57:55 -0300

    add react location

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { ActionsView } from './components/ActionsView';
-import { IntentsView } from './components/IntentsView';
-import { StoryView } from './components/StoryView';
-import { DndContext } from './contexts/DndContext';
-import { useActions } from './hooks/useActions';
+import { ReactLocation, Router, Outlet, Link } from '@tanstack/react-location';
+import { StoryBuilder } from './pages/StoryBuilder';
+
+const location = new ReactLocation();
+
+const routes = [{ path: '/', element: <StoryBuilder /> }];
 
 export const App = () => {
   return (
-    <main className="grid grid-cols-10 h-full">
-      <DndContext>
-        <ActionsView className="col-start-1 col-end-3 shadow-2xl" />
-        <StoryView className="col-start-3 col-end-9 mx-24 my-12" />
-        <IntentsView className="col-start-9 col-end-11 shadow-2xl" />
-      </DndContext>
-    </main>
+    <Router location={location} routes={routes}>
+      <Outlet />
+    </Router>
   );
 };
---
+++ src/pages/StoryBuilder.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ActionsView } from '../components/ActionsView';
+import { IntentsView } from '../components/IntentsView';
+import { StoryView } from '../components/StoryView';
+import { DndContext } from '../contexts/DndContext';
+
+export const StoryBuilder = () => {
+  return (
+    <main className="grid grid-cols-10 h-full pt-8">
+      <DndContext>
+        <ActionsView className="col-start-1 col-end-3 shadow-2xl" />
+        <StoryView className="col-start-3 col-end-9 mx-24 my-4" />
+        <IntentsView className="col-start-9 col-end-11 shadow-2xl" />
+      </DndContext>
+    </main>
+  );
+};

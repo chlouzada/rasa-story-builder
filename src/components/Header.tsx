88d7ac15a7ae1ac0f3f9d990e55c3f0346a36c84
carlos <chlouzada@gmail.com>
2022-09-29 19:11:17 -0300

    nav item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import { Link } from '@tanstack/react-location';
 
+const NavItem = ({
+  to,
+  children,
+  exact,
+}: {
+  to: string;
+  children: React.ReactNode;
+  exact?: true;
+}) => {
+  const activeProps = () => {
+    return {
+      style: {
+        fontWeight: 'bold',
+      },
+    };
+  };
+
+  return (
+    <Link
+      to={to}
+      activeOptions={{ exact }}
+      className="py-1 px-2 text-white"
+      getActiveProps={activeProps}
+    >
+      {children}
+    </Link>
+  );
+};
+
 const Navigation = () => {
   return (
-    <div>
-      <Link to="/" activeOptions={{ exact: true }}>
-        Home
-      </Link>
-      <Link to="posts">Actions</Link>
+    <div className="flex md:gap-3">
+      <NavItem to="/" exact>
+        Builder
+      </NavItem>
+      <NavItem to="/config">Config</NavItem>
     </div>
   );
 };
 
 export const Header = () => {
   return (
-    <div className="p-2 py-4 bg-primary flex w-full absolute">
-      <p className="mr-auto font-extrabold text-white">Rasa Story Builder</p>
+    <div className="p-2 bg-primary flex items-center w-full absolute shadow-md">
+      <p className="mr-auto font-extrabold text-white md:text-xl">
+        Rasa Story Builder
+      </p>
       <Navigation />
     </div>
   );
